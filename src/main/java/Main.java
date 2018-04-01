59aa899624c15602773055ecb109c73f00c52955
rumpel <artem.sinkovskiy@gmail.net>
2018-04-02 00:10:30 +0300

    Save(product name) to db from xml

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,21 @@
 import models.User;
 
+import org.xml.sax.SAXException;
 import service.UserService;
+import xml.parse_xml.PathToElement;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathExpressionException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ParserConfigurationException, SAXException, XPathExpressionException, IOException {
         UserService userService = new UserService();
+        PathToElement pathToElement = new PathToElement();
 
         User user = new User();
-            user.setFirst_name("test_first_n123123123123123");
+            user.setFirst_name(pathToElement.getName());
             user.setLast_name("test_last_name2");
             user.setPhone(932071888);
             user.setSex("man");
@@ -16,12 +23,14 @@ public class Main {
         //create
         userService.add(user);
         //read
-        System.out.println(userService.getAll());
-        System.out.println(userService.getNameById(3));
+        //System.out.println(userService.getAll());
+        //System.out.println(userService.getNameById(3));
         //update
-        userService.update(user);
+        //userService.update(user);
         //delete
-        userService.delete(user);
+        //userService.delete(user);
+        //new PathToElement();
+        userService.closeTransactionSession();
 
     }
 }
